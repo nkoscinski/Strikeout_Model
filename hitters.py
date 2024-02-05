5821bbd9238fe10b328391afb09548206f5791ac
nkoscinski <nick@wingx-advance.com>
2024-02-05 17:35:26 -0500

    ruff format

--- hitters.py
+++ hitters.py
@@ -2,7 +2,20 @@ import pandas as pd
 from scraping import dataframes
 
 # List of all possible pitch counts
-pitch_counts = ["0_0", "0_1", "0_2", "1_0", "2_0", "3_0", "1_1", "1_2", "2_1", "2_2", "3_1", "3_2"]
+pitch_counts = [
+    "0_0",
+    "0_1",
+    "0_2",
+    "1_0",
+    "2_0",
+    "3_0",
+    "1_1",
+    "1_2",
+    "2_1",
+    "2_2",
+    "3_1",
+    "3_2",
+]
 
 # Columns to convert
 columns_to_convert = [
@@ -30,24 +43,30 @@ for pitch_count in pitch_counts:
     # Pair up batter and pitcher dataframes
     pairs = [
         (dataframes[batter_key], dataframes[pitcher_key])
-        for batter_key in keys if batter_key.startswith("batter")
-        for pitcher_key in keys if pitcher_key.startswith("pitcher")
+        for batter_key in keys
+        if batter_key.startswith("batter")
+        for pitcher_key in keys
+        if pitcher_key.startswith("pitcher")
     ]
 
     # Perform the conversion on each pair
     for batter_df, pitcher_df in pairs:
         for df in [batter_df, pitcher_df]:
             for column in columns_to_convert:
-                df[column] = pd.to_numeric(df[column].str.strip("%"), errors="coerce") / 100.0
+                df[column] = (
+                    pd.to_numeric(df[column].str.strip("%"), errors="coerce") / 100.0
+                )
 
         # Perform element-wise multiplication on each pair and store the results
-        result_df = batter_df[columns_to_convert].multiply(pitcher_df[columns_to_convert])
+        result_df = batter_df[columns_to_convert].multiply(
+            pitcher_df[columns_to_convert]
+        )
         result_df.loc["Sum"] = result_df.sum(axis=0)
         result_df = result_df.dropna(how="all")
 
         # Append the result to the all_results list
         all_results.append(result_df)
 
-# Print all resulting dataframes
-for pitch_count, result_df in zip(pitch_counts, all_results):
-    print(f"Result DataFrame {pitch_count}:\n{result_df}\n")
+if "__main__" == __name__:
+    for pitch_count, result_df in zip(pitch_counts, all_results):
+        print(f"Result DataFrame {pitch_count}:\n{result_df}\n")
