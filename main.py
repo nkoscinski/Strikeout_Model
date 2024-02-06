bcbfe25b0893722efd19bea900b7bab282defe50
nkoscinski <nick@wingx-advance.com>
2024-02-06 10:26:49 -0500

    Adds additional batters in main.py

--- main.py
+++ main.py
@@ -13,3 +13,108 @@ dataframes = scrape_data(batter_url, pitcher_url, batter_handedness)
 transition_matrix = calculate_transition_matrix(dataframes)
 
 total_probability = calculate_total_probability(transition_matrix)
+print(f"Batter 1 Total probaility: {total_probability}")
+
+batter_url = "https://www.brooksbaseball.net//h_tabs.php?player=462101&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=01/01/2023&endDate=01/01/2024&balls={}&strikes={}&b_hand=R"
+
+pitcher_url = "https://www.brooksbaseball.net//tabs.php?player=543135&p_hand=-1&ppos=-1&cn=200&compType=none&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=03/30/2007&endDate=11/11/2023&balls={}&strikes={}&b_hand={}"
+
+batter_handedness = "R"
+
+dataframes = scrape_data(batter_url, pitcher_url, batter_handedness)
+
+transition_matrix = calculate_transition_matrix(dataframes)
+
+total_probability = calculate_total_probability(transition_matrix)
+print(f"Batter 2 Total probaility: {total_probability}")
+
+batter_url = "https://www.brooksbaseball.net//h_tabs.php?player=656514&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=01/01/2023&endDate=01/01/2024&balls={}&strikes={}&b_hand=R"
+
+pitcher_url = "https://www.brooksbaseball.net//tabs.php?player=543135&p_hand=-1&ppos=-1&cn=200&compType=none&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=03/30/2007&endDate=11/11/2023&balls={}&strikes={}&b_hand={}"
+
+batter_handedness = "L"
+
+dataframes = scrape_data(batter_url, pitcher_url, batter_handedness)
+
+transition_matrix = calculate_transition_matrix(dataframes)
+
+total_probability = calculate_total_probability(transition_matrix)
+print(f"Batter 3 Total probaility: {total_probability}")
+
+batter_url = "https://www.brooksbaseball.net//h_tabs.php?player=547989&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=01/01/2023&endDate=01/01/2024&balls={}&strikes={}&b_hand=R"
+
+pitcher_url = "https://www.brooksbaseball.net//tabs.php?player=657757&p_hand=-1&ppos=-1&cn=200&compType=none&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=03/30/2007&endDate=11/11/2023&balls={}&strikes={}&b_hand={}"
+
+batter_handedness = "L"
+
+dataframes = scrape_data(batter_url, pitcher_url, batter_handedness)
+
+transition_matrix = calculate_transition_matrix(dataframes)
+
+total_probability = calculate_total_probability(transition_matrix)
+print(f"Batter 4 Total probaility: {total_probability}")
+
+batter_url = "https://www.brooksbaseball.net//h_tabs.php?player=518735&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=01/01/2023&endDate=01/01/2024&balls={}&strikes={}&b_hand=R"
+
+pitcher_url = "https://www.brooksbaseball.net//tabs.php?player=543135&p_hand=-1&ppos=-1&cn=200&compType=none&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=03/30/2007&endDate=11/11/2023&balls={}&strikes={}&b_hand={}"
+
+batter_handedness = "L"
+
+dataframes = scrape_data(batter_url, pitcher_url, batter_handedness)
+
+transition_matrix = calculate_transition_matrix(dataframes)
+
+total_probability = calculate_total_probability(transition_matrix)
+print(f"Batter 5 Total probaility: {total_probability}")
+
+batter_url = "https://www.brooksbaseball.net//h_tabs.php?player=543198&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=01/01/2023&endDate=01/01/2024&balls={}&strikes={}&b_hand=R"
+
+pitcher_url = "https://www.brooksbaseball.net//tabs.php?player=543135&p_hand=-1&ppos=-1&cn=200&compType=none&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=03/30/2007&endDate=11/11/2023&balls={}&strikes={}&b_hand={}"
+
+batter_handedness = "L"
+
+dataframes = scrape_data(batter_url, pitcher_url, batter_handedness)
+
+transition_matrix = calculate_transition_matrix(dataframes)
+
+total_probability = calculate_total_probability(transition_matrix)
+print(f"Batter 6 Total probaility: {total_probability}")
+
+batter_url = "https://www.brooksbaseball.net//h_tabs.php?player=457705&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=01/01/2023&endDate=01/01/2024&balls={}&strikes={}&b_hand=R"
+
+pitcher_url = "https://www.brooksbaseball.net//tabs.php?player=543135&p_hand=-1&ppos=-1&cn=200&compType=none&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=03/30/2007&endDate=11/11/2023&balls={}&strikes={}&b_hand={}"
+
+batter_handedness = "R"
+
+dataframes = scrape_data(batter_url, pitcher_url, batter_handedness)
+
+transition_matrix = calculate_transition_matrix(dataframes)
+
+total_probability = calculate_total_probability(transition_matrix)
+print(f"Batter 7 Total probaility: {total_probability}")
+
+batter_url = "https://www.brooksbaseball.net//h_tabs.php?player=120074&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=01/01/2023&endDate=01/01/2024&balls={}&strikes={}&b_hand=R"
+
+pitcher_url = "https://www.brooksbaseball.net//tabs.php?player=543135&p_hand=-1&ppos=-1&cn=200&compType=none&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=03/30/2007&endDate=11/11/2023&balls={}&strikes={}&b_hand={}"
+
+batter_handedness = "L"
+
+dataframes = scrape_data(batter_url, pitcher_url, batter_handedness)
+
+transition_matrix = calculate_transition_matrix(dataframes)
+
+total_probability = calculate_total_probability(transition_matrix)
+print(f"Batter 8 Total probaility: {total_probability}")
+
+batter_url = "https://www.brooksbaseball.net//h_tabs.php?player=121347&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=01/01/2023&endDate=01/01/2024&balls={}&strikes={}&b_hand=R"
+
+pitcher_url = "https://www.brooksbaseball.net//tabs.php?player=543135&p_hand=-1&ppos=-1&cn=200&compType=none&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=03/30/2007&endDate=11/11/2023&balls={}&strikes={}&b_hand={}"
+
+batter_handedness = "R"
+
+dataframes = scrape_data(batter_url, pitcher_url, batter_handedness)
+
+transition_matrix = calculate_transition_matrix(dataframes)
+
+total_probability = calculate_total_probability(transition_matrix)
+print(f"Batter 9 Total probaility: {total_probability}")
