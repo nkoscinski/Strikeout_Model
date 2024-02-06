1fb5d00e407bf33c8b0f8de60f33857eb9d1466b
nkoscinski <nick@wingx-advance.com>
2024-02-06 10:05:18 -0500

    Adds scrape_data() function into main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-"""
-Currently this main.py file is empty, but it will be used to run the entire project.
-"""
+from scraping import scrape_data
+from hitters import calculate_transition_matrix
+from markov import calculate_total_probability
+
+batter_url = "https://www.brooksbaseball.net//h_tabs.php?player=547989&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=01/01/2023&endDate=01/01/2024&balls={}&strikes={}&b_hand=R"
+
+pitcher_url = "https://www.brooksbaseball.net//tabs.php?player=543135&p_hand=-1&ppos=-1&cn=200&compType=none&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=03/30/2007&endDate=11/11/2023&balls={}&strikes={}&b_hand={}"
+
+batter_handedness = "R"
+
+scrape_data(batter_url, pitcher_url, batter_handedness)
