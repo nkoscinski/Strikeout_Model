26e186ee0eaff3076acefd2405da9f2ce82afd13
nkoscinski <nick@wingx-advance.com>
2024-02-06 10:55:14 -0500

    Changes date in pitcher_url

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ batters = [
     # Add more batters as needed
 ]
 
-pitcher_url = "https://www.brooksbaseball.net//tabs.php?player=543135&p_hand=-1&ppos=-1&cn=200&compType=none&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=03/30/2007&endDate=11/11/2023&balls={}&strikes={}&b_hand={}"
+pitcher_url = "https://www.brooksbaseball.net//tabs.php?player=543135&p_hand=-1&ppos=-1&cn=200&compType=none&gFilt=&time=month&minmax=ci&var=po&s_type=2&startDate=01/01/2023&endDate=01/01/2024&balls={}&strikes={}&b_hand={}"
 
 times_faced = 3  # The number of times the pitcher faces each batter
 total_strikeouts = 0  # Initialize the total strikeouts
